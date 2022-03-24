d1b468abcf3b1aa3afd8e224f2d0eb4233a37e0c
Radoslav Rác <racradoslav96@gmail.com>
2022-03-24 08:33:30 +0100

    removed unnessesery code from html, added comments to  script.js

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,6 +1,5 @@
 const main = document.querySelector('#main');
 const lists = document.querySelectorAll('.rating__list');
-const active = document.querySelector('.active');
 const button = document.querySelector('#submit');
 const thankYou = document.querySelector('#thank_you');
 const message = document.querySelector('span');
@@ -8,6 +7,7 @@ const message = document.querySelector('span');
 lists.forEach(list => {
     list.addEventListener('click', (e) => {
 
+        // Save active class to const active
         const active = document.querySelector('.active');
 
         // Check if there is any active and remove it
@@ -15,6 +15,7 @@ lists.forEach(list => {
             active.classList.remove('active');
         }
 
+        // Save event target to const target for better code clarity
         const target = e.target;
 
         // Add active class
@@ -23,10 +24,14 @@ lists.forEach(list => {
         // Save rating to variable
         let rating = target.innerText;
 
+        // Add event listener (click) to submit button
         button.addEventListener('click', (e) => {
+
+            // Handle sections visibility
             main.style.display = "none";
             thankYou.style.display = "block";
 
+            // Change span text in thankYou section
             message.innerText = `You selected ${rating} of 5`;
         })
     })
